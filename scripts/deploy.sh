f7be41359ebeb26a1edada58bc6b3226e1672eba
Noah Citron <noah@jeff.org>
2021-11-18 20:39:09 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 OWNER=0xd24BD0D0bF4E30e91cE904f4DCD400b8dA06a1bf
 
-#HOLON=$(dapp create HolonToken $OWNER)
-! dapp verify-contract src/HolonToken.sol:HolonToken 0x3c2e12f800D893B5A417cc3a1C5E412F0f38F80f $OWNER
+HOLON=$(dapp create HolonToken $OWNER)
+! dapp verify-contract src/HolonToken.sol:HolonToken $HOLON $OWNER
