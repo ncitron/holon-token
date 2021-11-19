296ab5ff4e652b35dc1e4a881a0c6493880c6dee
ncitron <noah@jeff.org>
2021-11-18 20:14:26 -0500

    add token and tests

--- src/HolonToken.t.sol
+++ src/HolonToken.t.sol
@@ -5,18 +5,39 @@ import "ds-test/test.sol";
 
 import "./HolonToken.sol";
 
+contract Owner {
+    function mint(HolonToken _token, address _to, uint256 _amount) external {
+        _token.mint(_to, _amount);
+    }
+}
+
+contract User {
+    function mint(HolonToken _token, address _to, uint256 _amount) external {
+        _token.mint(_to, _amount);
+    }
+}
+
 contract HolonTokenTest is DSTest {
+
     HolonToken token;
+    Owner owner;
+    User user;
 
     function setUp() public {
-        token = new HolonToken();
+        owner = new Owner();
+        user = new User();
+
+        token = new HolonToken(address(owner));
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function test_mint() public {
+        owner.mint(token, address(0x1), 52 ether);
+
+        assertEq(token.balanceOf(address(0x1)), 52 ether);
+        assertEq(token.totalSupply(), 52 ether);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testFail_mintNotOwner() public {
+        user.mint(token, address(0x1), 53 ether);
     }
 }
