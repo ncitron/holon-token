59e883f0a79381caa3ceb69b7ec736414324f493
Noah Citron <noah@jeff.org>
2021-11-18 20:15:25 -0500

    add token contract (#2)
    * dapp install https://github.com/OpenZeppelin/openzeppelin-contracts.git
    
    * remove bad oz
    
    * dapp install https://github.com/OpenZeppelin/openzeppelin-contracts
    
    * add token and tests

--- src/HolonToken.sol
+++ src/HolonToken.sol
@@ -1,5 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-contract HolonToken {
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import { ERC20Votes } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract HolonToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
+
+    constructor(address _owner) ERC20("Holon", "HOLON") ERC20Permit("Holon") {
+        _transferOwnership(_owner);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    /* ========= Internal Functions ========== */
+
+    function _afterTokenTransfer(
+        address _from,
+        address _to,
+        uint256 _amount
+    )
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(_from, _to, _amount);
+    }
+
+    function _mint(address _to, uint256 _amount) internal override(ERC20, ERC20Votes) {
+        super._mint(_to, _amount);
+    }
+
+    function _burn(address _account, uint256 _amount) internal override(ERC20, ERC20Votes) {
+        super._burn(_account, _amount);
+    }
 }
